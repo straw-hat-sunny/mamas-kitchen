d481d3409ba39fb2ed422bb89ca747cd819f7825
suraj swarnapuri <suraj-swarnapuri@github.com>
2025-01-27 21:25:37 -0600

    implement translator worker

---
+++ worker/translator_worker.py
@@ -0,0 +1,33 @@
+from worker import Worker 
+import logging
+import json
+from translator import Translator, LocalTranslator
+
+logging.basicConfig(level=logging.INFO)
+
+class TranslatorWorker(Worker):
+    def __init__(self, translator:Translator,recieve_queue_name, max_msg, send_queue_name):
+        super().__init__(recieve_queue_name,max_msg,send_queue_name)
+        self.translator = translator
+
+    def process(self,msg):
+        logging.info(msg)
+        if 'text' not in msg:
+            logging.error(f"Message does not contain 'text' property\n {msg}")
+            return
+
+        recipe_text = msg['text']
+        logging.info(f"Processing recipe: {recipe_text}")
+        try:
+            recipe_object = translator.run(recipe_text)
+            logging.info(recipe_object)
+            processed_msg = json.dumps({"status":"complete"})
+            return processed_msg
+        except Exception as e:
+            logging.info("translator failed")
+            logging.error(e)
+            return
+
+translator = LocalTranslator()
+tw = TranslatorWorker(translator, "blob-transcribed", 1, "blob-translated")
+tw.run()
--- worker/worker.py
+++ worker/worker.py
@@ -3,7 +3,7 @@ import time
 import json
 from abc import ABC, abstractmethod
 from az import QueueService
-
+import logging
 
 
 # Worker listens to a queue and dequeues messages given a max number, performs a function, deletes the message, and sends a message to another queue
@@ -22,13 +22,16 @@ class Worker(ABC):
 
     def run(self):
         while True:
-            messages = self.recieve_queue_client.receive_messages(max_messages=self.max_msgs)
-            for message in messages:
-                msg_content = json.loads(message.content)
-                processed_msg = self.process(msg_content)
-                # if there are no errors processing the message then we can delete the message from the queue and send message to the send_aueue
-                self.recieve_queue_client.delete_message(message)
-                self.send_queue_client.send_message(processed_msg)
-                
+            try:
+                messages = self.recieve_queue_client.receive_messages(max_messages=self.max_msgs)
+                for message in messages:
+                    msg_content = json.loads(message.content)
+                    processed_msg = self.process(msg_content)
+                    # if there are no errors processing the message then we can delete the message from the queue and send message to the send_aueue
+                    self.recieve_queue_client.delete_message(message)
+                    self.send_queue_client.send_message(processed_msg)
+            except Exception as e:
+                 logging.info(e)
+                    
             time.sleep(15)
     
